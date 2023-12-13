16ca7c5fed45b68363ab19a123cb1c9b47062a54
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2023-12-13 12:58:47 +0530

    Encapsulation part done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        //rwOnly.name = "yash";
+        //System.out.println(rwOnly.name);
+        // name has private access in com.driver.RWOnly.
+
+        rwOnly.setName("yash");
+        System.out.println(rwOnly.getName());
+    }
 }
